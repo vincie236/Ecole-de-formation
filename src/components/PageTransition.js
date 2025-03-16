7dc191bc49ecf94c34691376a6c9a9dabdf91315
Jolly KETA <vincentiaketamaboudi@gmail.com>
2025-03-16 22:35:50 +0100

    Creation des composants utiles Navbar, Footer, PageTransition, ScrollToTopButton, ThemeToggle

---
+++ src/components/PageTransition.js
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const PageTransition = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default PageTransition;
